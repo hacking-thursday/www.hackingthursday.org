92155a1694276be0cb524ec959b31683d5595fd4
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2024-10-21 22:57:40 +0800

    feat: add build script and update Makefile
    - Introduced `.make.sh` script with build, build-local, and help commands.
    - Added `regenerate_menu_yml` function to generate the menu from weekly notes.
    - Updated `Makefile` to use `.make.sh` for build and build-local commands.
    
    Signed-off-by: Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>

---
+++ .make.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+SELF=$(readlink -f $0)
+ROOT_DIR=$(dirname $SELF)
+
+function usage() {
+	cmd=$(basename $SELF)
+	cat <<EOD
+usage:
+	$cmd help          # show this message
+	$cmd build         # run build script
+	$cmd build-local   # build jekyll site locally with docker/podman
+
+EOD
+}
+
+function regenerate_menu_yml() {
+	cat <<EOD
+- label: Latest
+  items:
+    - name: HackMD
+      link: "https://pad.hackingthursday.org"
+EOD
+	for yr in $(seq 2008 $(date +"%Y") | sort -r); do
+		cat <<EOD
+- label: $yr
+  items:
+EOD
+
+		(
+			cd $ROOT_DIR
+			find weeklynote/ -type f -name "*.md"
+		) | grep -e "$yr-" | sort -r | while read -r line; do
+			item=$line
+			item=${item#weeklynote/}
+			item=${item%.md}
+			cat <<EOD
+    - name: $item
+      link: "/weeklynote/$item"
+EOD
+		done
+
+	done
+}
+
+case "$1" in
+build)
+	# Regenerate weeklynotes' sidebar listing
+	# Scan `/weeklynote` files and update `/_data/menu.yml`
+	regenerate_menu_yml >$ROOT_DIR/_data/menu.yml
+
+	;;
+
+build-local)
+	$SELF build
+
+	CONTAINER_CLI="docker"
+
+	# Use podman instead if available
+	(which podman >&/dev/null) && {
+		CONTAINER_CLI="podman"
+	}
+
+	[ -n "$JEKYLL_GITHUB_TOKEN" ] && {
+		OPTS="$OPTS -e JEKYLL_GITHUB_TOKEN=$JEKYLL_GITHUB_TOKEN "
+	}
+
+	[ -n "$JEKYLL_LOG_LEVEL" ] && {
+		OPTS="$OPTS -e JEKYLL_LOG_LEVEL=$JEKYLL_LOG_LEVEL "
+	}
+
+	time (
+		$CONTAINER_CLI run \
+			--rm \
+			-it \
+			-v "${PWD}:/srv/jekyll:Z" \
+			-e JEKYLL_ROOTLESS=1 \
+			-e BUNDLE_APP_CONFIG='.bundle' \
+			$OPTS \
+			docker.io/jekyll/jekyll \
+			sh -c "bundle update && bundle exec jekyll build --incremental"
+	)
+	;;
+
+help | *)
+	usage
+	;;
+
+esac
