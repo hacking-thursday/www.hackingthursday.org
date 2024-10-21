2e7d70f09dd9b08d7840d6d4c3adc47ef88921e2
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2024-10-21 23:37:37 +0800

    feat: automate weekly note processing and cleanup config
    - Added `process_weeklynotes` function to `.make.sh` for automating the processing of weekly notes:
      - Generates front matter for each markdown file in `weeklynote/`.
      - Validates file names for correct date format.
      - Creates corresponding Jekyll `_posts` files with metadata and content.
    - Removed redundant settings in `_config.yml` related to `weeklynote`.
    - Deleted the outdated `_data/menu.yml` file.
    
    Signed-off-by: Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>

--- .make.sh
+++ .make.sh
@@ -43,12 +43,47 @@ EOD
 	done
 }
 
+function process_weeklynotes() {
+	tmpd=$(mktemp -d)
+	(
+		cd $ROOT_DIR || exit
+		find weeklynote/ -maxdepth 1 -type f -name "*.md" | while read -r line; do
+			echo "Processing $line ..."
+			fpath="$line"
+			date_str=$(echo "$fpath" | sed -e "s/^.*weeklynote\///g" -e "s/\.md$//g")
+
+			(echo $date_str | grep -e "^[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}$" >&/dev/null) || {
+				echo "[SKIP] $date_str is not in correct format."
+				continue
+			}
+			cat >$tmpd/front_matter.txt <<-EOD
+				---
+				title: "$date_str 聚會手記"
+				date: $date_str
+				layout: post
+				permalink: /weeklynote/$date_str
+				show_sidebar: false
+				menubar: menu
+				category: weeklynote
+				---
+			EOD
+			dst_fpath="$ROOT_DIR/_posts/${date_str}-聚會手記.md"
+			install -D /dev/null "$dst_fpath"
+			cat $tmpd/front_matter.txt >>"$dst_fpath"
+			cat "$fpath" >>"$dst_fpath"
+		done
+	)
+	rm -r $tmpd
+}
+
 case "$1" in
 build)
 	# Regenerate weeklynotes' sidebar listing
 	# Scan `/weeklynote` files and update `/_data/menu.yml`
 	regenerate_menu_yml >$ROOT_DIR/_data/menu.yml
 
+	process_weeklynotes
+	exit
 	;;
 
 build-local)
